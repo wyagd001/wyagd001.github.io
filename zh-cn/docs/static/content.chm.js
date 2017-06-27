7750fd33f18d02777b5e0da30f08b67630bbca4a
wyagd001 <wyagd001@163.com>
2017-06-27 15:49:48 +0800

    -_-

--- zh-cn/docs/static/content.chm.js
+++ zh-cn/docs/static/content.chm.js
@@ -15,7 +15,7 @@ translate = {
   cdDownloadBtn: "下载",
   verToolTipAHK_L: "Applies to:\nAutoHotkey_L Revision {0} and later\nAutoHotkey v1.0.90.00 and later",
   verToolTipDefault: "Applies to AutoHotkey {0} and later",
-  tutLocalMessage: "如果你是在本地查看此文档, 你可能已经安装了 AutoHotkey 并且可以跳过 b 部分."
+  tutLocalMessage: "Since you're viewing this documentation locally, you've probably already installed AutoHotkey and can skip to section b."
 };
 
 if (!IsInsideCHM() && !IsSearchBot())
