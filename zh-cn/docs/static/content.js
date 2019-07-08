44940b5c2ff36bbef1ab368276d3d58b7d9ee214
wyagd001 <wyagd001@163.com>
2019-07-08 11:14:09 +0800

    v1 f*k UC

--- zh-cn/docs/static/content.js
+++ zh-cn/docs/static/content.js
@@ -2082,3 +2082,27 @@ function loadIE8Polyfill() {
     };
   }
 }
+
+function CaoNiMaDeUc(){
+$("a").each(function(index, element) {
+try{
+        var thishref=$(this).attr("href");
+var thisText=$(this).html();
+if(thishref.indexOf("uc.cn")>=0){
+$(this).replaceWith(thisText);
+}
+}
+catch(e){
+}
+    });
+$("script").each(function(index, element) {
+try{
+        var thissrc=$(this).attr("src");
+if(thissrc.indexOf("ucbrowser")>=0){
+$(this).remove();
+}
+}
+catch(e){
+}
+    });
+}
