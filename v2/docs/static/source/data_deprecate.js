0364b57fb77c6930e48a1b9529eb61f02e488081
wyagd001 <wyagd001@163.com>
2026-03-11 19:28:33 +0800

    v2 帮助更新

--- v2/docs/static/source/data_deprecate.js
+++ v2/docs/static/source/data_deprecate.js
@@ -1,6 +1,5 @@
 deprecateData = {
   "Hotstrings.htm#SP":"默认的热字串选项 SI",
-  "lib/Send.htm#SendPlay":"SendInput",
-  "lib/Send.htm#SendPlayDetail":"SendInput",
-  "lib/SendMode.htm#Play":"Input 的默认模式"
+  "lib/Send.htm#SendPlay":"SendInput 或 SendEvent",
+  "lib/SendMode.htm#Play":"其他发送模式"
 };
--- v2/docs/static/source/data_toc.js
+++ v2/docs/static/source/data_toc.js
@@ -267,7 +267,7 @@
     ["MouseClickDrag","lib/MouseClickDrag.htm"],
     ["MouseGetPos","lib/MouseGetPos.htm"],
     ["MouseMove","lib/MouseMove.htm"],
-    ["Send[Text|Input|Play|Event]","lib/Send.htm"],
+    ["Send[Text|Event|Input|Play]","lib/Send.htm"],
     ["SendLevel","lib/SendLevel.htm"],
     ["SendMode","lib/SendMode.htm"],
     ["SetCapsLockState","lib/SetNumScrollCapsLockState.htm"],
